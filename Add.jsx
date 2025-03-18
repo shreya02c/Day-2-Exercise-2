1721e2ad9d8693f1b9373ea2b78f96e6923b4f1f
shreya02c <shreyachaudhari996@gmail.com>
2025-03-18 11:54:01 +0530

    Add files via upload

---
+++ Add.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Input from './Input';
+
+function App() {
+    return (
+        <div id="content">
+            <Input type="text" placeholder="Your name" />
+            <Input richText placeholder="Your message" />
+        </div>
+    )
+}
+
+export default App;
